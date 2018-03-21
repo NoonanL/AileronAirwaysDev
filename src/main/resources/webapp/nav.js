f5a15c5e40a049e777695d497280c794fa40560a
MWorms200 <36006031+MWorms200@users.noreply.github.com>
2018-03-21 15:59:56 +0000

    Changes to index.html and Timelines.html

--- src/main/resources/webapp/nav.js
+++ src/main/resources/webapp/nav.js
@@ -6,12 +6,21 @@ function openNav() {
 /* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+
+    //add the sidenav button to the page
+    document.body.innerHTML += '<span class="openbtn" onclick="openNav()" style="font-size: 40px; color: white">Please open </span>';
 }
 //add the sidenav div to the body of the page
 document.body.innerHTML += '<div id="mySidenav" class="sidenav">\n' +
     '        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()" style="font-size: 40px">&times;</a>\n' +
     '    </div>';
 var sideNavContent = document.getElementById("mySidenav");
+
+//Add an anchor tag to the list
+var aTag = document.createElement('a');
+aTag.setAttribute('href',"index.html");
+aTag.innerHTML = "Home";
+sideNavContent.appendChild(aTag);
 
 //Add an anchor tag to the list
 var aTag = document.createElement('a');
