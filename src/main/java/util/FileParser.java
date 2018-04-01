89a4f34bcd3b676d7a93c4981a1542a1574a14a9
Liam Noonan <contactliamnoonan@gmail.com>
2018-04-01 21:50:53 +0100

    More work on fileparser for attachments sent from html. Nae joy. Think the issue lies in how the data is being passed as it ends up being unreadable at the serverside leading to corrupt image data.

--- src/main/java/util/FileParser.java
+++ src/main/java/util/FileParser.java
@@ -1,7 +1,14 @@
 package util;
 
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.IOUtils;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.io.*;
-import java.util.Arrays;
+
+
 
 public class FileParser {
 
@@ -12,21 +19,52 @@ public class FileParser {
         String fileExtension = ".";
         fileExtension = (fileExtension + ext.substring(ext.lastIndexOf(".") + 1));
         System.out.println(fileExtension);
-
         StringBuffer sbf = new StringBuffer();
         sbf.append(inputString);
         String dest = "new_file" + fileExtension;
 
-        BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(dest)));
+        //Checks for filetype of the new path some error checking might be good here for conflicting types
+        if (fileExtension.equals(".doc") || fileExtension.equals(".txt")) {
 
-        //write contents of StringBuffer to a file
-        bwr.write(sbf.toString());
+            BufferedWriter bwr = new BufferedWriter(new FileWriter(new File(dest)));
 
-        //flush the stream
-        bwr.flush();
+            //write contents of StringBuffer to a file
+            bwr.write(sbf.toString());
 
-        //close the stream
-        bwr.close();
+            //flush the stream
+            bwr.flush();
+
+            //close the stream
+            bwr.close();
+        }
+        else if (fileExtension.equals(".png")){
+            InputStream stream = IOUtils.toInputStream(inputString, "UTF-8");
+            BufferedImage image = ImageIO.read(stream);
+            ImageIO.write(image, "png", new File(dest));
+        }
+        else if(fileExtension.equals(".jpg")|| fileExtension.equals(".jpeg")){
+            OutputStream outputStream = null;
+            byte [] imageInByteArray = Base64.decodeBase64(
+                    inputString);
+            try {
+                outputStream = new FileOutputStream("new_file.jpeg");
+                outputStream.write(imageInByteArray);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }finally{
+                try {
+                    outputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+
 
         System.out.println("Content of StringBuffer written to File.");
 
