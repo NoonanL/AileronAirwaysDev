2746953ae44cfda06a44b95c580abc363dd4c045
Scott Thompson <36005491+Scott-Was-Taken@users.noreply.github.com>
2018-04-02 16:05:03 +0100

    fixed deletetimelineform into seperate form
    deletetimeline was pushing into timelineform because Im a silly goose. Fixed this

--- src/main/resources/webapp/confirmRequired.js
+++ src/main/resources/webapp/confirmRequired.js
@@ -1,3 +1,4 @@
+console.log("the confirmRequired Script is in use");
 $('.confirmRequired').submit(function() {
     var c = confirm("Are you sure? This action cannot be undone");
     console.log(c);
