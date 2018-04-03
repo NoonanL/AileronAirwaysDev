0c750062be1c7525752bd6a5d0060ae4c5acb203
Scott Thompson <36005491+Scott-Was-Taken@users.noreply.github.com>
2018-04-03 16:59:53 +0100

    changed filesize popup to be a little bit more professional

--- src/main/resources/webapp/attachMaxSize.js
+++ src/main/resources/webapp/attachMaxSize.js
@@ -3,8 +3,8 @@ var uploadField = document.getElementById("attachments");
 uploadField.onchange = function() {
     //if the file is larger than 2MB
     if(this.files[0].size > 2097152){
-        //tell the user its too big
-        alert("File is too big!");
+        //tell the user its too big and they can select another file
+        alert("Max file size is 2MB, please select another file.");
         //reset the inputbox value
         this.value = "";
     };
