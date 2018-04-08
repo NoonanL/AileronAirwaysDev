637c64cd213cb511a904c92d10bc80b853325f97
Liam Noonan <contactliamnoonan@gmail.com>
2018-04-08 19:31:04 +0100

    added John's fetchLinkedEvents code

--- src/main/java/util/FetchLinkedEvents.java
+++ src/main/java/util/FetchLinkedEvents.java
@@ -3,31 +3,48 @@ package util;
 import application.Runner;
 import application.model.Event;
 import application.model.Timeline;
-import com.google.gson.Gson;
+import application.repositories.TimelineRepository;
 
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FetchLinkedEvents {
 
-    public String fetchLinkedEvents(String timelineId){
+    ArrayList<String> linkedArrayList;
 
-        Timeline timeline = Runner.timelineRepository.get(timelineId);
-        ArrayList<Event> events = timeline.getTimelineEvents();
+    public void getAllLinks(String id) throws UnsupportedEncodingException {
+        //TimelineRepository timelineRepository = new TimelineRepository();
+        //Runner.timelineRepository.getAllTimelinesAndEvents();
+        List<ArrayList<Event>> list = new ArrayList<>();
+//
+//        //get the timeline that the id points to
+        Timeline testTimeline = Runner.timelineRepository.get(id);
+//        //get the events on that timeline
+        ArrayList<Event> testData = testTimeline.getTimelineEvents();
+//
 
-        for(Event e : events){
-            ArrayList linkedIds = e.getLinkedEvents();
-            for(Object s : linkedIds){
-                for(Event compareEvent : events){
-                    if(compareEvent.getLinkedEvents().contains(s)){
 
-                    }
+        for (Event event:testData
+                ) {
+            ArrayList<Event> events = new ArrayList<>();
+            //Add current event to the start of the arraylist unsure if needed but looked that way
+            events.add(event);
+            if(event.getLinkedEvents() != null){
+
+                ArrayList<String> arrayList = event.getLinkedEvents();
+                //System.out.println(arrayList.toString());
+
+                for (String linkedeventId : arrayList
+                        ) {
+                    Event event1 = Runner.eventRepository.get(linkedeventId);
+                    events.add(event1);
+
                 }
-
             }
+            list.add(events);
         }
-
-        String json = new Gson().toJson("test");
-        return json;
+        System.out.println(list.toString());
     }
-
 }
+
